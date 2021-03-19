4f760de996435086705c5323a2906d510873d7ed
Aleksandra Lubicka <48988356+alubiss@users.noreply.github.com>
2021-03-20 00:14:45 +0100

    zmiana danych

--- kriging.R
+++ kriging.R
@@ -41,24 +41,30 @@ dane$`liczba gwiazdek` = as.numeric(gsub(",", ".", dane$`liczba gwiazdek`, fixed
 dane$`średni spędzany czas (min)`=as.numeric(dane$`średni spędzany czas (min)`)
 
 # dane punktowe
-coordinates(firmy) <- ~coords.x1+coords.x2
-bubble(firmy, "zatr", maxsize=2.5, main ="Lokalizacja punktów", key.entries=5*c(1,6,30,120,300), alpha=1/2)
+dane.sp <- dane
+coordinates(dane.sp) <- c("x","y") 
+#proj4string(dane.sp) <- CRS("+proj=longlat +datum=NAD83")
+#dane.sp <- spTransform(dane.sp, CRS("+proj=longlat +datum=NAD83"))
+
+bubble(dane.sp, "N", maxsize=2.5, main ="Lokalizacja punktów", key.entries=1000*c(2,4,5,6), alpha=1/2)
 
 # utworzenie zmiennej losowej z rozkładu jednostajnego [0,1] 
-firmy$r<-runif(dim(firmy)[1])
+dane.sp$r<-runif(dim(dane.sp)[1])
 # nowe obiekty są klasy SpatialPointsDataFrame:
-input <- firmy[firmy$r<0.8, ] # wybranie 80% danych na zbiór treningowy 
-output <- firmy[firmy$r>0.8, ] # wybranie 20% danych na zbiór testowy
+input <- dane.sp[dane.sp$r<0.8, ] # wybranie 80% danych na zbiór treningowy 
+output <- dane.sp[dane.sp$r>0.8, ] # wybranie 20% danych na zbiór testowy
 
-woj <-readOGR(".","wojewodztwa") # 16 jedn. 
-woj.df<-as.data.frame(woj) 
-region<-woj[woj.df$jpt_nazwa_=="lubelskie",] 
-region<-spTransform(region, CRS("+proj=longlat +datum=WGS84")) 
-W <- as(region, "owin")
+pow<-readOGR("/Users/alubis/Desktop/OneDrive/mag/Powiaty", "Powiaty")
+#pow<-spTransform(pow, CRS("+proj=longlat +datum=NAD83"))
+#pow2 =pow[pow@data[["JPT_NAZWA_"]]=="powiat Warszawa",]
+pow.df<-as.data.frame(pow) 
+pow2<-pow[pow.df$jpt_nazwa_=="powiat Warszawa",]
+pow2<-spTransform(pow2, CRS("+proj=longlat +datum=WGS84"))
+W<-as(pow2, "owin")
 
 # wykres krigingu
 plot(W, main="In-sample and out-of-sample data") 
-points(input, pch=16)
-points(output, col="red", add=TRUE, pch=16) 
+points(input)
+points(output, col="red", add=TRUE) 
 legend("left", legend=c("input data", "output data"),col=c("black", "red"), pch=16)
 
