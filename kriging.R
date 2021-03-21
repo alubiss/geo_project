5a0086a2efd7e56c76f48e460b70faa6e22447f0
Aleksandra Lubicka <48988356+alubiss@users.noreply.github.com>
2021-03-21 22:51:08 +0100

    obliczenie błędów rmse

--- kriging.R
+++ kriging.R
@@ -55,16 +55,57 @@ input <- dane.sp[dane.sp$r<0.8, ] # wybranie 80% danych na zbiór treningowy
 output <- dane.sp[dane.sp$r>0.8, ] # wybranie 20% danych na zbiór testowy
 
 pow<-readOGR("/Users/alubis/Desktop/OneDrive/mag/Powiaty", "Powiaty")
-#pow<-spTransform(pow, CRS("+proj=longlat +datum=NAD83"))
-#pow2 =pow[pow@data[["JPT_NAZWA_"]]=="powiat Warszawa",]
-pow.df<-as.data.frame(pow) 
-pow2<-pow[pow.df$jpt_nazwa_=="powiat Warszawa",]
-pow2<-spTransform(pow2, CRS("+proj=longlat +datum=WGS84"))
+pow<-spTransform(pow, CRS("+proj=longlat +datum=NAD83"))
+pow2 =pow[pow@data[["JPT_NAZWA_"]]=="powiat Warszawa",]
+pow2<-spTransform(pow2, CRS("+proj=merc +datum=NAD83"))
 W<-as(pow2, "owin")
 
 # wykres krigingu
 plot(W, main="In-sample and out-of-sample data") 
-points(input)
-points(output, col="red", add=TRUE) 
+points(input@coords , pch=16)
+points(output@coords, col="red", pch=16, add=TRUE) 
 legend("left", legend=c("input data", "output data"),col=c("black", "red"), pch=16)
 
+library(automap)
+# zwykły kriging (ordinary kriging) 
+ok.var <- autofitVariogram(N~1, input) 
+plot(ok.var)
+# uniwersalny kriging oparty na funkcji współrzędnych Kartezjańskich 
+uk.var.1 <- autofitVariogram(N~x+y, input) 
+plot(uk.var.1)
+# uniwersalny kriging oparty na odległości od centrum
+#uk.var.2 <- autofitVariogram(N~, input) 
+#plot(uk.var.2)
+
+## zwykły kriging
+ok.xy <- autoKrige(N~1, input, output, verbose=FALSE)
+## uniwersalny kriging, x+y
+uk.xy <- autoKrige(N~x+y, input, output, verbose=FALSE)
+## uniwersalny kriging, dist
+#uk.dist <- autoKrige(N~dist, input, output, verbose=FALSE)
+
+## original values
+result0<-automapPlot(output, "N", main="Original \n data") 
+result0
+
+## ordinary
+result1<-automapPlot(ok.xy$krige_output, "var1.pred", main="Ordinary
+kriging") 
+result1
+
+## universal, x+y
+result2<-automapPlot(ok.xy$krige_output, "var1.pred", main="Universal
+kriging, \n Cartesian coordinates function") 
+result2
+
+## universal, dist
+#result3<-automapPlot(ok.xy$krige_output, "var1.pred", main="Universal kriging, \n distance to Lublin") 
+#result3
+
+library(Metrics)
+rmse_ord <- rmse(output$N, ok.xy$krige_output@data$var1.pred) 
+rmse_un_xy <- rmse(output$N, uk.xy$krige_output@data$var1.pred) 
+#rmse_un_dist <- rmse(output$N, uk.dist$krige_output@data$var1.pred)
+
+rmse_ord
+rmse_un_xy
